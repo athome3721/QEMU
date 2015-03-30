5f966deb35dca5f3cc4659f6bdcc1f248f81adf9
qiaochong <qiaochong@loongson.cn>
2015-03-30 16:48:18 +0800

    change ls232e prid.
    Signed-off-by: qiaochong <qiaochong@loongson.cn>

--- target-mips/translate_init.c
+++ target-mips/translate_init.c
@@ -174,7 +174,7 @@ static const mips_def_t mips_defs[] =
         /* A generic CPU providing MIPS64 Release 2 features.
            FIXME: Eventually this should be replaced by a real CPU model. */
         .name = "ls232e",
-        .CP0_PRid = 0x16307,
+        .CP0_PRid = 0x14230,
         .CP0_Config0 = /*MIPS_CONFIG0 | (0x1 << CP0C0_AR) | (0x2 << CP0C0_AT) |
                        (MMU_TYPE_R4000 << CP0C0_MT)*/ 0x80000480,
         .CP0_Config1 = /*MIPS_CONFIG1 | (1 << CP0C1_FP) | (63 << CP0C1_MMU) |
